13179a0495c15109bb947a4e7fccfb8ba6d9a588
hippolytecosserat <hippolytecosserat@gmail.com>
2025-01-06 09:30:29 +0100

    Nom correct

--- src/main/java/linguacrypt/Main.java
+++ src/main/java/linguacrypt/Main.java
@@ -1,4 +1,4 @@
-package album;
+package linguacrypt;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
--- src/main/java/linguacrypt/controleur/DemoControleur.java
+++ src/main/java/linguacrypt/controleur/DemoControleur.java
@@ -1,4 +1,4 @@
-package album.controleur;
+package linguacrypt.controleur;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
