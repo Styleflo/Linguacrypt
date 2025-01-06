aa3b977836d712616d626a9eba34e1a3f9ed28d3
Marthouret <ely.marthouret@telecomnancy.eu>
2025-01-06 17:23:59 +0100

    Qrcode généré à partir d'une string

--- src/main/java/linguacrypt/model/Clef.java
+++ src/main/java/linguacrypt/model/Clef.java
@@ -2,6 +2,16 @@ package linguacrypt.model;
 
 import java.util.*;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
 public class Clef {
     private final boolean blueStarts;
     private final int[][] grid;
@@ -128,5 +138,12 @@ public class Clef {
         }
 
         return res;
+    }
+    public void String_to_qrcode(String text) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, 300, 300);
+
+        Path path = new File("../../../ressources/assets/").toPath();
+        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
     }
 }
