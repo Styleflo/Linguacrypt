da6bfc5fb580ae5cc3fdcf628a7be52597db0844
Florian Touraine <florian.touraine@gmail.com>
2025-01-06 09:49:37 +0100

    [+] getter, setter Plateau

--- src/main/java/linguacrypt/model/Jeu.java
+++ src/main/java/linguacrypt/model/Jeu.java
@@ -1,4 +1,15 @@
 package linguacrypt.model;
 
 public class Jeu {
+    private Plateau plateau;
+
 }
+
+public Plateau getPlateau() {
+    return this.plateau;
+}
+
+public void setPlateau(Plateau plateau) {
+    this.plateau = plateau;
+}
+
