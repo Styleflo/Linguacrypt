004141f6d0c62de8bd2029e5f90a2fa33f988fb6
Florian Touraine <florian.touraine@gmail.com>
2025-01-06 10:03:11 +0100

    [+] Jeu

--- src/main/java/linguacrypt/model/Jeu.java
+++ src/main/java/linguacrypt/model/Jeu.java
@@ -3,16 +3,17 @@ package linguacrypt.model;
 public class Jeu {
     private Plateau plateau;
 
-}
 
-public Jeu(Plateau plateau) {
-    this.plateau = plateau;
-}
+    public Jeu(Plateau plateau) {
+        this.plateau = plateau;
+    }
 
-public Plateau getPlateau() {
-    return this.plateau;
-}
+    public Plateau getPlateau() {
+        return this.plateau;
+    }
 
-public void setPlateau(Plateau plateau) {
-    this.plateau = plateau;
+    public void setPlateau(Plateau plateau) {
+        this.plateau = plateau;
+    }
+
 }
