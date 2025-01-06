0b035bb5602b6e9076a3d2716929426cfa38393e
hippolytecosserat <hippolytecosserat@gmail.com>
2025-01-06 09:48:05 +0100

    Debut de carte

--- src/main/java/linguacrypt/model/Carte.java
+++ src/main/java/linguacrypt/model/Carte.java
@@ -1,4 +1,29 @@
 package linguacrypt.model;
 
 public class Carte {
+    private String word;
+    private int type;  // 0: bleu ; 1: rouge ; 2: blanc ; 3: noir
+
+    public Carte(String word, int type) {
+        this.word = word;
+        this.type = type;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+
 }
