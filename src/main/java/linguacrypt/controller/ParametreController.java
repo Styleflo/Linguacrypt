9e2a3e390b4658b32975875dd99dbe442a5c9dfa
Maï <mai.morvan@telecomnancy.eu>
2025-01-07 15:26:40 +0100

    le temps de checkout

---
+++ src/main/java/linguacrypt/controller/ParametreController.java
@@ -0,0 +1,16 @@
+package linguacrypt.controller;
+
+import linguacrypt.model.Jeu;
+
+public class ParametreController {
+
+    private Jeu jeu;
+
+    public ParametreController(Jeu jeu) {
+        this.jeu = jeu;
+    }
+
+    public ParametreController(){}
+
+
+}
