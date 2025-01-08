4b8aaf164b4e92d816cb13ad9636fc91034b981c
e0297u <zoe.le-gleut@telecomnancy.eu>
2025-01-08 15:57:44 +0100

    encore des filtres

--- src/main/java/linguacrypt/controller/MenuInitialController.java
+++ src/main/java/linguacrypt/controller/MenuInitialController.java
@@ -3,6 +3,7 @@ package linguacrypt.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import linguacrypt.model.Jeu;
 import linguacrypt.model.Partie;
@@ -19,28 +20,36 @@ public class MenuInitialController implements Observer {
     }
 
     public void setJeu(Jeu jeu) {
+        filtre.setMouseTransparent(true);
         this.jeu = jeu;
     }
 
     @FXML
+    private ImageView filtre;
+
+    @FXML
     private void handleJouerButtonAction() throws IOException {
         jeu.setView("Plateau");
+
         PartieBuilder partieBuilder = new PartieBuilder(jeu);
         Partie partie = partieBuilder.getResult();
         jeu.setPartie(partie);
         jeu.notifyObservers();
+
     }
 
     @FXML
     private void handleCartesButtonAction() {
         jeu.setView("Cartes");
         jeu.notifyObservers();
+
     }
 
     @FXML
     private void handleQuitterButtonAction(ActionEvent event) {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.close();
+
     }
 
     @Override
