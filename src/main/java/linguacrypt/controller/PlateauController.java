29efd0f618553e94671caab6d1b31b423c28edaa
e0297u <zoe.le-gleut@telecomnancy.eu>
2025-01-08 15:10:28 +0100

    je suis fatiguée

--- src/main/java/linguacrypt/controller/PlateauController.java
+++ src/main/java/linguacrypt/controller/PlateauController.java
@@ -48,6 +48,29 @@ public class PlateauController implements Observer {
 
 
     private void afficherCartes() {
+
+        if(jeu.getPartie().getPlateau().isBlueTurn()){
+            imageview1.setVisible(true);  // Si visible, devient inv
+            imageview2.setVisible(false);  // Si visible, devient inv
+            panneau_changer.getStyleClass().clear(); // Supprimer toutes les classes existantes
+            panneau_changer.getStyleClass().add("main_panneau");
+            panneau_changer.getStyleClass().add("blue_main_panneau");
+            panneau_changer2.getStyleClass().clear();
+            panneau_changer2.getStyleClass().add("logo_panneau_bleu");
+            panneau_changer2.getStyleClass().add("logo_panneau");
+        }
+        else{
+
+            imageview1.setVisible(false);  // Si visible, devient inv
+            imageview2.setVisible(true);  // Si visible, devient inv
+            panneau_changer.getStyleClass().clear(); // Supprimer toutes les classes existantes
+            panneau_changer.getStyleClass().add("main_panneau");
+            panneau_changer.getStyleClass().add("red_main_panneau");
+            panneau_changer2.getStyleClass().clear();
+            panneau_changer2.getStyleClass().add("logo_panneau_rouge");
+            panneau_changer2.getStyleClass().add("logo_panneau");
+        }
+
         if (jeu == null) return;
 
         gridPane.getChildren().clear();
