00f4666144a81984dcc765e4de3b1c9a033806c3
Maï <mai.morvan@telecomnancy.eu>
2025-01-09 15:43:13 +0100

    Hop c est centre

--- src/main/java/linguacrypt/controller/ImageCardController.java
+++ src/main/java/linguacrypt/controller/ImageCardController.java
@@ -23,11 +23,7 @@ public class ImageCardController {
             try {
                 Image image = new Image(url, false); // true pour le chargement en arrière-plan
                 cardImage.setImage(image);
-
-                cardImage.setPreserveRatio(true); // Conserve les proportions de l'image
-                cardImage.setFitWidth(cardImage.getBoundsInParent().getWidth()); // Ajuste à la largeur du conteneur
-                cardImage.setFitHeight(cardImage.getBoundsInParent().getHeight()); // Ajuste à la hauteur du conteneur
-                cardImage.setSmooth(true);
+                cardImage.setPreserveRatio(true);
 
                 currentUrl = url;
             } catch (Exception e) {
