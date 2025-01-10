b36a45821ed3a7a564cba09380f6c7b2b86df710
hippolytecosserat <hippolytecosserat@gmail.com>
2025-01-10 14:24:52 +0100

    Préparation de la persistance des statistiques

--- src/main/java/linguacrypt/utils/UserConfig.java
+++ src/main/java/linguacrypt/utils/UserConfig.java
@@ -6,6 +6,7 @@ public class UserConfig {
     private List<WordsCategory> categories;
     private List<String> usedImages;
     private List<String> addedImages;
+    private Statistics statistics;
 
     public List<WordsCategory> getCategories() {
         return categories;
@@ -17,5 +18,9 @@ public class UserConfig {
 
     public List<String> getAddedImages() {
         return addedImages;
+    }
+
+    public Statistics getStatistics() {
+        return statistics;
     }
 }
